befa66eb86fbc19a820dcff46d39690dc021051f
Sara Soltan <sarasoltanm2000@gmail.com>
2022-03-21 13:38:18 +0330

    step_2

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: const HomePage(),
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView(),
+    },
   ));
 }
 
@@ -21,27 +25,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print("you are a verified user!");
-              } else {
-                print("you are not a verified user!");
-              }
-              return const Text("done");
-            default:
-              return const Text("Loading...");
-          }
-        },
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            // final user = FirebaseAuth.instance.currentUser;
+            // if (user?.emailVerified ?? false) {
+            //   return const Text("done");
+            // } else {
+            //   return VerifyEmailView();
+            // }
+            return const LoginView();
+          default:
+            return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
