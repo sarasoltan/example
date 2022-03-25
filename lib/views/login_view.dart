7b3f3aba67e01c638de186e9c7603296ca0f1378
Sara Soltan <sarasoltanm2000@gmail.com>
2022-03-25 22:03:50 +0430

    step_4

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -61,16 +62,17 @@ class _LoginViewState extends State<LoginView> {
                 final userCredential = await FirebaseAuth.instance
                     .signInWithEmailAndPassword(
                         email: email, password: password);
-                print(userCredential);
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/notes/', (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  print("user not found");
+                  devtools.log("user not found");
                 } else if (e.code == "wrong-password") {
-                  print("wrong password");
+                  devtools.log("wrong password");
                 } else if (e.code == "too-many-requests") {
-                  print("too many requests");
+                  devtools.log("too many requests");
                 } else {
-                  print(e.code);
+                  devtools.log(e.code);
                 }
               }
             },
